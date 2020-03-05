57480cf5e8b41320917837109fa14da77041ffe5
Daneliz Urena <durena402@gmail.com>
2020-03-04 21:17:21 -0500

    this is really bad right now

--- js/brickBreaker.js
+++ js/brickBreaker.js
@@ -1,31 +1,45 @@
 var canvas = document.getElementById("board");
 var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 80;
+var canvasWidth = 500;
+var canvasHeight = 600;
+var ballRadius = 12.5;
+var x = canvasWidth / 2;
+var y = canvasHeight - 30;
+var dx = 2;
+var dy = -2;
 var paddleHeight = 5;
 var paddleWidth = 50;
-var paddlex = (canvas.width - paddleWidth) / 2;
+var paddleX = (canvasWidth - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
-//var ballRadius=10;
-var brickRowNum = 4;
+var brickRowNum = 6;
 var brickColumnNum = 5;
-var count = brickRowNum * brickColumnNum;
-var rem = count;
-var score = document.getElementById("score");
-var lives = document.getElementById("lives")
-var brickWidth = 10;
-var brickHeight = 15;
-var brickPadding = 7;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 40;
-var bricks=[];
-  for(z = 0;z < brickColumnNum; ++z){
-    bricks[z] = [];
-    for(a = 0; a < brickRowNum;++a){
-      bricks[a][z]= {x:0, y:0, status:1};
-    }
+
+var brickWidth = 50;
+var brickHeight = 10;
+var brickPadding = 5;
+var brickOffsetTop = 10;
+var brickOffsetLeft = 10;
+var score = 0;
+var lives = 3;
+
+var bricks = [];
+for(z = 0;z < brickColumnNum; ++z){
+  bricks[z] = [];
+  for(a = 0; a < brickRowNum;++a){
+    bricks[z][a]= {x:0, y:0, status:1};
   }
-  var dx= 3.5;
-  var dy= -3.5;
-  
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
